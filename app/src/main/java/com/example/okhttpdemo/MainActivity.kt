bfd9335da7fa33f14fa982d684270b017bfbdc3c
Youssef Soliman <yosfsoliman151@gmail.com>
2023-04-19 19:03:40 +0200

    change the cloth a day

--- app/src/main/java/com/example/okhttpdemo/MainActivity.kt
+++ app/src/main/java/com/example/okhttpdemo/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.okhttpdemo
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.okhttpdemo.data.ClothHelper
 import com.example.okhttpdemo.data.Clothes
@@ -12,6 +11,8 @@ import com.example.okhttpdemo.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
     private val PREFS_NAME = "MyPrefsFile"
@@ -31,9 +32,13 @@ class MainActivity : AppCompatActivity() {
         getDataFromApi()
 
         val savedData = sharedPreferences.getInt("id", 0)
+        val savedDate = sharedPreferences.getString("date","")
+        val newDate=getDate()
+        if(isSameDate(savedDate,newDate)){
         if (savedData != 0) {
             val cloth = ClothHelper.clothesList.first { it.id == savedData }
             binding.image.setImageResource(cloth.imageId)
+        }
         }
 
     }
@@ -46,8 +51,6 @@ class MainActivity : AppCompatActivity() {
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
-                Log.i("bkprogrammer", "fail")
-
             }
 
             @Throws(IOException::class)
@@ -55,28 +58,27 @@ class MainActivity : AppCompatActivity() {
                 response.body?.string()?.let { jsonString ->
                     val result = Gson().fromJson(jsonString, NationalResponse::class.java)
 
-                    
+
                     runOnUiThread {
                         binding.textData.text =
                             result.data.timelines[0].intervals[0].values.temperature.toString()
                         val hotOrCold =
-                            check(result.data.timelines[0].intervals[0].values.temperature)
-                        val resultOfSelectingOneCloth =
-                            getCloth(ClothHelper.clothesList, hotOrCold).random().id
+                            checkHotOrColdBasedOnTemprature(result.data.timelines[0].intervals[0].values.temperature)
+                        val idOfSelectingOneCloth =
+                            getListOfClothUsedOnSummerOrWinter(ClothHelper.clothesList, hotOrCold).random().id
 
                         val editor = sharedPreferences.edit()//like open file i use .edit
-                        editor.putInt("id", resultOfSelectingOneCloth)
+                        editor.putInt("id", idOfSelectingOneCloth)
+                        editor.putString("date", getDate())
                         editor.apply()//close file
 
                     }
-
-
                 }
             }
         })
     }
 
-    fun check(temperature: Double): String {
+    fun checkHotOrColdBasedOnTemprature(temperature: Double): String {
         return when (temperature) {
             in 0.0..20.0 -> "cold"
             in 20.0..50.0 -> "warm"
@@ -86,12 +88,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun getCloth(list: List<Clothes>, type: String): List<Clothes> {
+    fun getListOfClothUsedOnSummerOrWinter(list: List<Clothes>, type: String): List<Clothes> {
         val result = list.filter {
             it.type == type
         }
         return result
     }
 
+    fun getDate(): String {
+        val date = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("d M yyyy")
+        val formattedDate = formatter.format(date)
 
+        return formattedDate.toString()
+    }
+
+    fun isSameDate(savedDate: String?, newDate: String): Boolean {
+        return when (savedDate?.compareTo(newDate)) {
+            0 -> true
+            else -> false
+        }
+    }
 }
